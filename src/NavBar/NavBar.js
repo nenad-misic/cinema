8ff420286674cbb1d2accada74476b143d6cdb80
Nenad Misic <nenad.misic997@gmail.com>
2021-10-10 19:05:01 +0200

    Fix copy link problem

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -3,15 +3,10 @@ import {Link} from 'react-router-dom';
 
 function NavBar() {
   return (
-    // <nav className="navbar navbar-dark bg-primary fixed-top">
-    //   <Link className="navbar-brand" to="/">
-    //     Cinemax
-    //   </Link>
-    // </nav>
 
     <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
     <Link className="navbar-brand" to="/">
-      Cinemax
+      Cinema
     </Link>
     <button className="navbar-toggler border" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
